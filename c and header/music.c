ade1d10ca502b1674b11c061d71b4a61b7674aae
Agronault <xande.luis@hotmail.com>
2019-09-20 23:30:51 -0300

    0.2fix
    [*]load fix

--- c and header/music.c
+++ c and header/music.c
@@ -5,23 +5,29 @@
 
 
 Mix_Chunk * load_music(char const file[40]){
-    return Mix_LoadWAV(file);
-}
-
-void play_music(Mix_Chunk * music, int channel, int loop) {
-    int canal;
-    int canal_audio = channel;
+    
+    int canal_audio = 2;
     int taxa_audio = 22050;
     Uint16 formato_audio = AUDIO_S16SYS;
     int audio_buffers = 4096;
 
     if (Mix_OpenAudio(taxa_audio, formato_audio, canal_audio, audio_buffers) != 0) {
         printf("Não pode inicializar audio: %s\n", Mix_GetError());
+        return NULL;
     }
+
+    Mix_Chunk* music= Mix_LoadWAV(file);
 
     if (music == NULL) {
         printf("Não foi possível inicializar audio: %s\n", Mix_GetError());
+        return NULL;
     }
+    
+    return music;
+}
+
+void play_music(Mix_Chunk * music, int channel, int loop) {
+    int canal;
 
     Mix_HaltChannel(channel);
     canal = Mix_PlayChannel(channel, music, loop);
